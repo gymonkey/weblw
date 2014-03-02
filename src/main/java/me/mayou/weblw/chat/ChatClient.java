ca2eeb0a694540e80b844f2bcde9850498c9b5e7
mayou <mayou@mayou-laptop.(none)>
2014-03-02 18:26:20 +0800

    测试第一个vertx程序

---
+++ src/main/java/me/mayou/weblw/chat/ChatClient.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
+ */
+package me.mayou.weblw.chat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.WebSocket;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * @author mayou.lyt
+ */
+public class ChatClient extends Verticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatClient.class);
+    
+    @Override
+    public void start() {
+        vertx.createHttpClient().setHost("10.125.48.74").setPort(8080).connectWebsocket("/", new Handler<WebSocket>() {
+            
+            @Override
+            public void handle(final WebSocket ws) {
+                ws.dataHandler(new Handler<Buffer>() {
+
+                    @Override
+                    public void handle(Buffer buf) {
+                        logger.info(buf.toString());
+                    }
+                });
+                ws.writeTextFrame("hello vertx");
+            }
+        });
+    }
+}
---
+++ src/main/java/me/mayou/weblw/chat/ChatServer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
+ */
+package me.mayou.weblw.chat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * @author mayou.lyt
+ */
+public class ChatServer extends Verticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatServer.class);
+
+    @Override
+    public void start() {
+        vertx.createHttpServer().websocketHandler(new Handler<ServerWebSocket>() {
+
+            @Override
+            public void handle(final ServerWebSocket ws) {
+                ws.dataHandler(new Handler<Buffer>() {
+
+                    @Override
+                    public void handle(Buffer buf) {
+                        ws.writeTextFrame(buf.toString());
+                    }
+                });
+            }
+        }).listen(9999, new Handler<AsyncResult<HttpServer>>() {
+
+            @Override
+            public void handle(AsyncResult<HttpServer> event) {
+                logger.info(String.valueOf(event.succeeded()));
+                if (!event.succeeded()) {
+                    logger.error(event.cause().getMessage());
+                }
+            }
+
+        });
+    }
+
+}
