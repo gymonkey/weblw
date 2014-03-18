8d209df16c8b9fac1568aacaa42b31e9695c0de8
mayou <mayou@mayou-laptop.(none)>
2014-03-19 00:21:02 +0800

    修复客户端心跳的问题

--- src/main/java/me/mayou/weblw/response/cmd/CreateResponseCmd.java
+++ src/main/java/me/mayou/weblw/response/cmd/CreateResponseCmd.java
@@ -63,11 +63,11 @@ public class CreateResponseCmd extends ResponseCmd {
             }
         });
         conn.setHeartbeatTimerId(heartbeatTimerId);
-        long readIdleTimerId = vertx.setPeriodic(60000, new Handler<Long>() {
+        long readIdleTimerId = vertx.setPeriodic(30000, new Handler<Long>() {
 
             @Override
             public void handle(Long timerId) {
-                if (System.currentTimeMillis() - conn.getLastReadTime() >= 60000) {
+                if (System.currentTimeMillis() - conn.getLastReadTime() >= 30000) {
                     conns.remove(conn.getId());
                     try {
                         conn.getWs().close();
